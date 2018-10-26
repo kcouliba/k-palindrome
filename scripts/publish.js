e270e9be361eca42ed7006e43fa8df6e3eef3f16
kevin coulibaly <kcoulibaly@b-flower.com>
2018-10-26 17:04:27 +0200

    kind of initial commit

---
+++ scripts/publish.js
@@ -0,0 +1,11 @@
+#!/usr/bin/nscript
+/* To run this script, nscript is needed: [sudo] npm install -g nscript
+/* Publish.js, publish a new version of the npm package as found in the current directory */
+module.exports = function(shell, git) {
+  const pkg = JSON.parse(shell.read('package.json'))
+
+  git('commit', '-am', 'Published version ' + pkg.version)
+  git('tag', pkg.version)
+  git('push')
+  git('push', '--tags')
+}
